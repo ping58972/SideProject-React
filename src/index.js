c92973b2cd552d80c7841135423594ebde7a0a2b
ping58972 <ping58972@gmail.com>
2019-05-02 13:31:52 -0500

    finished Section 5

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {latitude: "null"};
+        //Or this.state = {};
+        
 
-const App = () => {
-    return (
-        <div>Hello Ping!</div>
-    );
-};
+        window.navigator.geolocation.getCurrentPosition(
+            position =>{ this.setState({latitude: position.coords.latitude, longitude:  position.coords.longitude})}
+            , err => {
+                this.setState({errorMessage: err.message});
+            }
+        );
+    }
+    render() {
+      
+        
+            if (this.state.errorMessage &&!this.state.latitude ){ 
+                return <div> Error: {this.state.errorMessage} </div>
+            }
+            if(!this.state.errorMessage && this.state.latitude && this.state.longitude){
+                return (
+                <div>
+                <div> Latitude: {this.state.latitude}</div>
+                <div>longitude: {this.state.longitude} </div>
+                </div>
+                );
+            }
+            return (
+                <div>Loading...</div>
+            );
+          
+        
+    }
+}
 
 ReactDOM.render(
     <App/>, document.querySelector('#root')
